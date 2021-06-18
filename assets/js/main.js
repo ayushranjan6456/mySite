a08f77109efadefd44ca3ae0c557298e2a8f1a39
Ayush Ranjan <ayushranjan6456@gmail.com>
2021-06-18 21:19:28 +0530

    Mobile nav added

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,13 +1,23 @@
 /*==================== MENU SHOW Y HIDDEN ====================*/
-
+const navMenu = document.querySelector('#nav-menu'),
+      navToggle = document.querySelector('#nav-toggle'),
+      navClose = document.querySelector('#nav-close');
 
 /*===== MENU SHOW =====*/
 /* Validate if constant exists */
-
+if(navToggle){
+  navToggle.addEventListener('click', () => {
+    navMenu.classList.add('show-menu')
+  })
+}
 
 /*===== MENU HIDDEN =====*/
 /* Validate if constant exists */
-
+if(navClose){
+  navClose.addEventListener('click', () => {
+    navMenu.classList.remove('show-menu')
+  })
+}
 
 /*==================== REMOVE MENU MOBILE ====================*/
 
